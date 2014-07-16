b9f35864db1a3d16993c1ed9443bc68c264149e8
Andres Rodriguez <anrocox@gmail.com>
2014-07-16 10:59:38 -0500

    How to convert number data type to string in python?

---
+++ src/numbers_to_string_str.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 15, 2014
+
+@author: anroco
+
+How to convert number data type to string in python?
+
+¿Como convertir los datos tipo numero a string en python?
+'''
+
+#create a integer
+i = 482
+print(type(i))
+print(i)
+
+#convert using the str() function built-in python
+s = str(i)
+print(type(s))
+print(s)
+
+#create a float
+f = 273.837
+print(type(f))
+print(f)
+
+#convert using the str() function built-in python
+s = str(f)
+print(type(s))
+print(s)
